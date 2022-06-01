9e1fbdeee5ac9e76f6ba5ab39f0782b1dda5467d
MelvinE1999 <73800751+MelvinE1999@users.noreply.github.com>
2022-05-31 18:46:08 -0700

    update#1

---
+++ twitter_bot.py
@@ -0,0 +1,20 @@
+from time import strftime
+import stuff # holds my info
+import tweepy
+from textblob import TextBlob
+
+# Step 1 - Authenticate
+consumer_key= stuff.keys.get('con_key')
+consumer_secret= stuff.keys.get('con_sec')
+
+access_token= stuff.keys.get('api_tok')
+access_token_secret= stuff.keys.get('tok_key')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 3 - Retrieve Tweets
+public_tweets = api.search('Trump')
+
